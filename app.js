a99ad4eed0811e919c97209efa584c6dc9cbb556
mathiasVoelckerPUCRS <mathias.voelcker@acad.pucrs.br>
2018-11-14 17:21:51 -0200

    ajustar get e post para como está no exemplo

--- app.js
+++ app.js
@@ -9,21 +9,64 @@ const Yamaform = require('yamaform');
 const dbConfig = require('./config');
 
 const yamaform = new Yamaform(dbConfig, `${__dirname}/database.json`);
-const router = express.Router();
 
 
 generateTables = async () => {
     await yamaform.generateTables()
   }
 
-// generateTables();
+  let formClasses = {
+    'formClass':'', 
+    'labelClass':'', 
+    'inputClass':'form-control',
+    'inputWrapperClass':'form-group', 
+    'buttonClass':'btn btn-default pull-right'
+  }
+  
 
-router.get('/cliente', async () => {
-  let table = await yamaform.fetch('cliente', {'tableClass': 'cliente'});
-  console.log(table);
+  render = (html) => {
+    return '<html>'
+          +'<head>'
+          +'<title>Yamaform example</title>'
+          +'<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">'
+          +'</head>'
+          +'<body>'
+          +'<div class="container">'
+          +'<h1 class="text-center">Yamaform</h1>'
+          +html
+          +'</div>'
+          +'</body>'
+          +'</html>' 
+  }
+
+// generateTables();
+app.get('/cliente', async (req, res) => {
+
+  let table = await yamaform.fetch('cliente', {'tableClass':'table', 'viewUrl':'/cliente', 'deleteUrl':'/cliente/delete' })
+
+  res.set('Content-Type', 'text/html');
+  res.send(new Buffer(
+    render(
+      `<div class="row"><div class="col-md-12"><h3>Cliente</h3>${table}</div></div>`
+      +`<div class="row"><div class="col-md-12"><a href="/cliente/new" class="btn btn-primary pull-right">Add</a></div></div>`
+    )
+  ));
 });
 
-router.post('/cliente', async(req,res) => {
+
+app.get('/cliente/new', async (req, res) => {
+
+  let form = await yamaform.generateForm('cliente', {'method':'post', 'action':'/cliente', ...formClasses})
+  
+  res.set('Content-Type', 'text/html');
+  res.send(new Buffer(
+    render(
+      `<div class="row"><div class="col-md-12">${form}</div></div>`
+    )
+  ));
+});
+
+app.post('/cliente', async(req,res) => {
   console.log(req.body)
   let data = {
     "cliente":[
@@ -31,10 +74,7 @@ router.post('/cliente', async(req,res) => {
     ]
  }
   let table = await yamaform.insert(data);
-  // console.log(table);
-  return res;
+  res.redirect('/cliente')
 });
 
-app.use(bodyParser.json());
-app.use('/api', router);
 module.exports = app;
