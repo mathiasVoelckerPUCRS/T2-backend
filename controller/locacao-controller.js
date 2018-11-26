5b9fcb75a5821582fd38684609ab002a2b3dc177
mathiasVoelckerPUCRS <mathias.voelcker@acad.pucrs.br>
2018-11-26 20:11:37 -0200

    remover 1 mockado

--- controller/locacao-controller.js
+++ controller/locacao-controller.js
@@ -18,7 +18,7 @@ class LocacaoController {
                     "data_fim": req.body.data_fim,
                     "valor_total": req.body.valor_total,
                     "cliente_id": req.body.cliente,
-                    "veiculo_id": 1 // TODO: pedir correção para formulário atender 2 FK
+                    "veiculo_id": req.body.veiculo
                 },
               ]
            }
@@ -41,7 +41,7 @@ class LocacaoController {
                     "data_fim": req.body.data_fim,
                     "valor_total": req.body.valor_total,
                     "cliente_id": req.body.cliente,
-                    "veiculo_id": 1 // TODO: pedir correção para formulário atender 2 FK
+                    "veiculo_id": req.body.veiculo
                 },
               ]
         }
